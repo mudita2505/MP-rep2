f253f493c20d1fbbcef97c130149d2cc6573b3c6
mudita2505 <88099249+mudita2505@users.noreply.github.com>
2021-10-05 19:02:28 +0530

    Update settings.py

--- minor/settings.py
+++ minor/settings.py
@@ -53,6 +53,25 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
 ROOT_URLCONF = 'minor.urls'
 
 TEMPLATES = [
