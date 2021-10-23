e425b82873451727273af615e68396539f6f0e72
mudita2505 <88099249+mudita2505@users.noreply.github.com>
2021-10-23 19:42:37 +0530

    Update admin.py

--- app3/admin.py
+++ app3/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from app3.models import Question,Symptom,Option
+from app3.models import Question,Symptom,Option,Disease
 # Register your models here.
 @admin.register(Symptom)
 class SymptomAdmin(admin.ModelAdmin):
@@ -15,3 +15,7 @@ class QuestionAdmin(admin.ModelAdmin):
 @admin.register(Option)
 class OptionAdmin(admin.ModelAdmin):
     list_display=['options','opt']
+    
+@admin.register(Disease)
+class SymptomAdmin(admin.ModelAdmin):
+    list_display=['disease']
